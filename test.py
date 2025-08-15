3a7674f96db95b490778d4cf4627e3f0bf86d6ef
moyuhao-arch <739319163@qq.com>
2025-08-15 15:58:13 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
+import json
+
+from data_processing import DataProcessor
+from model import create_model
+
+def main():
+  config = {
+    'data_dir':'../DATASET/LIDC_patch_2'
+    'fold_structure':True,
+    'test_folds':[8,9],
+    'image_size': 64,
+    'normalize': True,
+    'model_path': './models/best_model.h5',
+    'batch_size': 32,
+    'save_dir': './test_results'
+  }
